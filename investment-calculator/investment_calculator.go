55d1d34458c6570cc92b2979d6a9f6e3af1f84c1
Jericho <jerichobantiquete@gmail.com>
2023-12-07 10:23:08 +0800

    add print after scan

--- investment-calculator/investment_calculator.go
+++ investment-calculator/investment_calculator.go
@@ -6,12 +6,19 @@ import (
 )
 
 func main() {
-	const inflationRate = 6.5
-	investmentAmount, years := 1000.0, 10.0
+	const inflationRate = 2.5
+	var investmentAmount, years float64
 	expectedReturnRate := 5.5
 
+	fmt.Print("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
-	
+
+	fmt.Print("Expected Return Date: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
 	fmt.Println(futureValue)
