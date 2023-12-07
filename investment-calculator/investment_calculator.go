393161da9b449ad7aefdd6f73e6ebc580612cf39
Jericho <jerichobantiquete@gmail.com>
2023-12-07 10:08:36 +0800

    ask user input for investment amount

--- investment-calculator/investment_calculator.go
+++ investment-calculator/investment_calculator.go
@@ -6,9 +6,11 @@ import (
 )
 
 func main() {
-	const inflationRate = 2.5
+	const inflationRate = 6.5
 	investmentAmount, years := 1000.0, 10.0
 	expectedReturnRate := 5.5
+
+	fmt.Scan(&investmentAmount)
 	
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
