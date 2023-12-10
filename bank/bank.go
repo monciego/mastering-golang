5ada1afe463e4b7d8f505b8663c68c3c2c7d92aa
Jericho <jerichobantiquete@gmail.com>
2023-12-10 23:11:29 +0800

    if statement for option 1

--- bank/bank.go
+++ bank/bank.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	var accountBalance float64 = 1000
+
 	fmt.Println("Welcome to Go Bank!")
 	fmt.Println("How can we help you today?")
 	fmt.Println("1. Check Account Balance")
@@ -13,4 +15,8 @@ func main() {
 	var choice int
 	fmt.Print("Please choose an option (1-4): ")
 	fmt.Scan(&choice)
+
+	if choice == 1 {
+		fmt.Println("Your balance is:", accountBalance)
+	}
 }
