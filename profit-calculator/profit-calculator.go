a2b4f176bc53f8d84d6bc8df430e10bfe128b9af
Jericho <jerichobantiquete@gmail.com>
2023-12-10 19:42:09 +0800

    refactor: profit calculator with function

--- profit-calculator/profit-calculator.go
+++ profit-calculator/profit-calculator.go
@@ -4,25 +4,47 @@ import "fmt"
 
 func main() {
 
-	var revenue float64
+	/* var revenue float64
 	var expenses float64
 	var taxRate float64
+ */
+	/* fmt.Print("Enter Revenue: ")
+	fmt.Scan(&revenue) */
 
-	fmt.Print("Enter Revenue: ")
-	fmt.Scan(&revenue)
+	 revenue := getUserInput("Enter Revenue: ")
 
-	fmt.Print("Enter Expenses: ")
-	fmt.Scan(&expenses)
+	/* fmt.Print("Enter Expenses: ")
+	fmt.Scan(&expenses) */
 
-	fmt.Print("Enter Tax Rate: ")
-	fmt.Scan(&taxRate)
+	expenses := getUserInput("Enter Expenses: ")
 
-	ebt := revenue - expenses
+/* 	fmt.Print("Enter Tax Rate: ")
+	fmt.Scan(&taxRate) */
+
+	taxRate := getUserInput("Enter Tax Rate: ")
+
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
+
+	/* ebt := revenue - expenses
 	profit := ebt * (1 - taxRate / 100)
-	ratio := ebt / profit
+	ratio := ebt / profit */
 
-	fmt.Println(ebt)
-	fmt.Println(profit)
-	fmt.Println(ratio)
+	fmt.Printf("%.1f\n", ebt)
+	fmt.Printf("%.1f\n", profit)
+	fmt.Printf("%.3f\n", ratio)
 
+}
+
+func getUserInput(infoText string) (float64) {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func calculateFinancials(revenue, expenses, taxRate float64) (ebt, profit, ratio float64) {
+	ebt = revenue - expenses
+	profit = ebt * (1 - taxRate / 100)
+	ratio = ebt / profit
+	return
 }
