1d82aacf1a991dbfe1c971c26213cfdd12587edd
Jericho <jerichobantiquete@gmail.com>
2023-12-10 23:48:36 +0800

    repeating code with for loop

--- bank/bank.go
+++ bank/bank.go
@@ -5,47 +5,49 @@ import "fmt"
 func main() {
 	var accountBalance float64 = 1000
 
-	fmt.Println("Welcome to Go Bank!")
-	fmt.Println("How can we help you today?")
-	fmt.Println("1. Check Account Balance")
-	fmt.Println("2. Make a Deposit")
-	fmt.Println("3. Withdraw Funds")
-	fmt.Println("4. Exit")
+	for i := 0; i < 2; i++ {
+		fmt.Println("Welcome to Go Bank!")
+		fmt.Println("How can we help you today?")
+		fmt.Println("1. Check Account Balance")
+		fmt.Println("2. Make a Deposit")
+		fmt.Println("3. Withdraw Funds")
+		fmt.Println("4. Exit")
 
-	var choice int
-	fmt.Print("Please choose an option (1-4): ")
-	fmt.Scan(&choice)
+		var choice int
+		fmt.Print("Please choose an option (1-4): ")
+		fmt.Scan(&choice)
 
-	if choice == 1 {
-		fmt.Println("Your balance is:", accountBalance)
-	} else if choice == 2 {
-		fmt.Print("How much do you want to deposit? ")
-		var depositAmount float64
-		fmt.Scan(&depositAmount)
+		if choice == 1 {
+			fmt.Println("Your balance is:", accountBalance)
+		} else if choice == 2 {
+			fmt.Print("How much do you want to deposit? ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
 
-		if depositAmount <= 0 {
-			fmt.Println("Invalid input for deposit amount. Must be greater than 0.")
-			return
+			if depositAmount <= 0 {
+				fmt.Println("Invalid input for deposit amount. Must be greater than 0.")
+				return
+			}
+
+			accountBalance += depositAmount
+			fmt.Println("Deposit successful. Your new balance is:", accountBalance)
+		} else if choice == 3 {
+			fmt.Print("How much do you want to widthdaw?")
+			var withdrawalAmount float64
+			fmt.Scan(&withdrawalAmount)
+
+			if withdrawalAmount <= 0 {
+				fmt.Println("Invalid input for deposit amount. Must be greater than 0.")
+				return
+			} else if withdrawalAmount > accountBalance {
+				fmt.Println("Insufficient funds. Cannot withdraw. ")
+				return
+			}
+
+			accountBalance -= withdrawalAmount
+			fmt.Println("Widthdaw successful. Your new balance is: ", accountBalance)
+		} else {
+			fmt.Println("Exiting. Thank you for using Go Bank!")
 		}
-
-		accountBalance += depositAmount
-		fmt.Println("Deposit successful. Your new balance is:", accountBalance)
-	} else if choice == 3 {
-		fmt.Print("How much do you want to widthdaw?")
-		var withdrawalAmount float64
-		fmt.Scan(&withdrawalAmount)
-
-		if withdrawalAmount <= 0 {
-			fmt.Println("Invalid input for deposit amount. Must be greater than 0.")
-			return
-		} else if withdrawalAmount > accountBalance {
-			fmt.Println("Insufficient funds. Cannot withdraw. ")
-			return
-		}
-
-		accountBalance -= withdrawalAmount
-		fmt.Println("Widthdaw successful. Your new balance is: ", accountBalance)
-	} else {
-		fmt.Println("Exiting. Thank you for using Go Bank!")
 	}
 }
