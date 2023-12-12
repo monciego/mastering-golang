42712c63fd9956818b3b9b21bc1e44a6a8b778f3
Jericho <jerichobantiquete@gmail.com>
2023-12-13 00:22:51 +0800

    validate user input and exit if input is invalid, store calculated result into a file

--- profit-calculator/profit-calculator.go
+++ profit-calculator/profit-calculator.go
@@ -1,50 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	revenue, err := getUserInput("Enter Revenue: ")
 
-	/* var revenue float64
-	var expenses float64
-	var taxRate float64
- */
-	/* fmt.Print("Enter Revenue: ")
-	fmt.Scan(&revenue) */
+	if err != nil {
+		fmt.Print("Error", err)
+		panic("Cannot continue due to errors")
+	}
 
-	 revenue := getUserInput("Enter Revenue: ")
+	expenses, err := getUserInput("Enter Expenses: ")
 
-	/* fmt.Print("Enter Expenses: ")
-	fmt.Scan(&expenses) */
+	if err != nil {
+		fmt.Print("Error", err)
+		panic("Cannot continue due to errors")
+	}
 
-	expenses := getUserInput("Enter Expenses: ")
+	taxRate, err := getUserInput("Enter Tax Rate: ")
 
-/* 	fmt.Print("Enter Tax Rate: ")
-	fmt.Scan(&taxRate) */
-
-	taxRate := getUserInput("Enter Tax Rate: ")
+	if err != nil {
+		fmt.Print("Error", err)
+		panic("Cannot continue due to errors")
+	}
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
-
-	/* ebt := revenue - expenses
-	profit := ebt * (1 - taxRate / 100)
-	ratio := ebt / profit */
 
 	fmt.Printf("%.1f\n", ebt)
 	fmt.Printf("%.1f\n", profit)
 	fmt.Printf("%.3f\n", ratio)
+	storeResultToFile(ebt, profit, ratio)
+}
+
+func storeResultToFile(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
 
 }
 
-func getUserInput(infoText string) (float64) {
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
-	fmt.Scan(&userInput)
-	return userInput
+	_, err := fmt.Scan(&userInput)
+	if err != nil {
+		return 0, fmt.Errorf("invalid input: %v", err)
+	}
+
+	if userInput <= 0 {
+		return 0, fmt.Errorf("invalid input: must be a positive non-zero value")
+	}
+
+	return userInput, nil
 }
+
 
 func calculateFinancials(revenue, expenses, taxRate float64) (ebt, profit, ratio float64) {
 	ebt = revenue - expenses
-	profit = ebt * (1 - taxRate / 100)
+	profit = ebt * (1 - taxRate/100)
 	ratio = ebt / profit
 	return
 }
