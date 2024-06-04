7efd6b61f8cfee8a29c9786921824c4df191d2c9
monciego <jerichobantiquete@gmail.com>
2024-06-04 18:17:35 +0800

    ch1 - introduction

---
+++ boot.dev/ch1-introduction/1welcomeToLearnGo.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func welcomeToLearnGo() {
+	// single-line comments start with "//"
+	// comments are just for documentation - they don't execute
+	fmt.Println("Starting Textio server...")
+}
---
+++ boot.dev/go.mod
@@ -0,0 +1,3 @@
+module github.com/monciego/mastering-golang/boot.dev
+
+go 1.21.5
